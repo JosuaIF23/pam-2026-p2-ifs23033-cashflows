9bc00aefccaf7362460e4f109a6b98c49fb59dc2
JosuaIF23 <firenoobgaming@gmail.com>
2026-02-11 09:51:09 +0700

    Second Done

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 val koin_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
-val ktor_version = "3.0.3" // Pastikan versi ini konsisten
+val ktor_version = "3.0.3"
 
 plugins {
-    // Gunakan versi 2.0.x atau 2.1.x yang stabil
     kotlin("jvm") version "2.1.0"
     id("io.ktor.plugin") version "3.0.3"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
@@ -13,34 +12,38 @@ plugins {
 group = "org.delcom"
 version = "0.0.1"
 
+// Mengatasi error Inconsistent JVM-target compatibility
+kotlin {
+    jvmToolchain(21)
+}
+
 application {
-    // Hapus ".kt", cukup nama class aslinya
     mainClass.set("org.delcom.ApplicationKt")
 }
 
 dependencies {
-    // Core Ktor & Netty
+    // Ktor Server Core & Netty
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
 
-    // --- TAMBAHKAN INI (Yang bikin error tadi) ---
+    // StatusPages & Serialization (Penting untuk tugas P2)
     implementation("io.ktor:ktor-server-status-pages-jvm:$ktor_version")
-    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
-    // ---------------------------------------------
-
-    // Serialization
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
 
-    // Koin
+    // Dotenv untuk membaca file .env
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
+
+    // Koin Dependency Injection
     implementation("io.insert-koin:koin-ktor:$koin_version")
     implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
 
-    // Others
+    // Fitur tambahan lainnya
     implementation("io.ktor:ktor-server-cors-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-config-yaml-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
+    // Testing
     testImplementation("io.ktor:ktor-server-test-host-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
